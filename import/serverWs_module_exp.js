a77452a5b51a40327d7d823e1755f2d10be203bb
nemo6 <miguel.maudet.contact@gmail.com>
2023-05-01 20:21:08 +0200

    Add files via upload

---
+++ import/serverWs_module_exp.js
@@ -0,0 +1,92 @@
+if (typeof exports !== "undefined") {
+	if (typeof module !== "undefined" && module.exports) {
+	module.exports = bar
+	}
+	exports._ = bar
+} else {
+  globalThis["serverWs"] = bar
+}
+
+function bar(x,n) {
+	require("dotenv").config()
+	global["RELOAD_TIME"] = 65
+	function makeid(x) {
+		let text = ""
+		let possible = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+		for ( let i=0;i<x;i++ ) text += possible.charAt( Math.floor( Math.random() * (possible.length + 1) ) )
+		return text
+	}
+	const expressSession = require("express-session")({
+	secret:process.env.SECRET,
+	resave:true,
+	saveUninitialized:true
+	})
+	const sharedsession = require("express-socket.io-session")
+	const express = require("express")
+	const cors    = require("cors")
+	const app     = express()
+	const server  = require("http").createServer(app)
+	const io      = require("socket.io")(server)
+	const PORT    = 8080
+	app.use(cors())
+	app.use(expressSession)
+	app.get("/",(req, res) => {
+		if ( !req.session.unique_id ){
+			req.session.unique_id = makeid(15)
+		}
+		res.writeHead(200,{"content-type":`text/${n};charset=utf8`})
+		res.end(`${x}
+		<script src="/socket.io/socket.io.js"></script>
+		<script>
+		let socket = io.connect(window.location.href)
+		socket.emit("browser_name", fnBrowserDetect() )
+		function fnBrowserDetect(){
+		let userAgent = navigator.userAgent
+		let browserName
+		if(userAgent.match(/chrome|chromium|crios/i)){
+		browserName = "Google chrome"
+		}else if(userAgent.match(/firefox|fxios/i)){
+		browserName = "Firefox"
+		}else if(userAgent.match(/safari/i)){
+		browserName = "Safari"
+		}else if(userAgent.match(/opr\//i)){
+		browserName = "Opera"
+		}else{
+		browserName="No browser detection"
+		}
+		return browserName
+		}
+		</script>
+		`)
+	})
+	server.listen(PORT)
+	io.use( sharedsession(expressSession) )
+	let hrstart=0
+	let reload=false
+	function promise_browser(socket){
+		return new Promise( resolve => {
+			socket.on("browser_name", (name) => {
+				resolve(name)
+			})
+		})
+	}
+	io.on("connection", async function (socket) {
+		if( hrstart != 0 ){
+			hrend = process.hrtime(hrstart)
+			reload=true
+			console.info("Execution time (hr): %ds %dms", hrend[0], hrend[1] / 1000000)
+			console.log(await promise_browser(socket),socket.handshake.session.unique_id)
+		}
+		socket.on("disconnect", function() {
+			hrstart = process.hrtime()
+			setTimeout( () => {
+				if(reload){
+					reload=false
+				}else{
+					process.exit(0)
+				}
+			}, RELOAD_TIME )
+		})
+	})
+	console.log(`Running at port ${PORT}`)
+}
